b08861c7cfb556b14d89be2ecf2ed27bb8dbd8df
Harshvardhan <93964573+Harshhvardhan@users.noreply.github.com>
2023-04-13 10:22:29 +0530

    updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
             </article>
           </div>
           <p>
-          As a web developer with a strong interest and proficiency in the frontend part of web development, I believe that my skills and experience would be an excellent fit for this role. I am also a hardworking individual who is committed to delivering high-quality work and always striving to give my 100 percent on every project.Throughout my career as a web developer, I have gained extensive experience in frontend development, including HTML, CSS, JavaScript, and various frontend frameworks like React. I am well-versed in building responsive and user-friendly web applications that provide an excellent user experience.
+           As a web developer with a strong interest and proficiency in the frontend part of web development, I believe that my skills and experience would be an excellent fit for this role. I am also a hardworking individual who is committed to delivering high-quality work and always striving to give my 100 percent on every project.Throughout my career as a web developer, I have gained extensive experience in frontend development, including HTML, CSS, JavaScript, and various frontend frameworks like React. I am well-versed in building responsive and user-friendly web applications that provide an excellent user experience.
           </p>
 
           <a href="#contact" className="btn btn-primary">Let's Talk</a>
